7c5e468d17e684a0edfd4dd0716b7d606ca0e82d
GG Learn <ozolsdeko@gmail.com>
2017-10-31 16:08:50 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,46 +1,42 @@
-WIN_COMBINATIONS = [
-  [0,1,2], # Top row
-  [3,4,5], # Middle row
-  [6,7,8], # Bottom row
-  [2,4,6], # Diognal from right
-  [0,4,8], # Diognal from left
-  [0,3,6], # Left column
-  [1,4,7], # Middle column
-  [2,5,8]  # Right column
-]
-
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
+
+# Define won?, full?, draw?, over?, and winner below
 def won?(board)
-  WIN_COMBINATIONS.each do |combo|
-  if board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X" ||
-      board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
-      return combo
-    end
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] 
   end
-  false
 end
 
 def full?(board)
-  return true if board.all? {|a| a.match(/[XO]/)}
-  false
+  board.all?{|token| token == "X" || token == "O"}
 end
 
 def draw?(board)
-  return true if full?(board) && won?(board) == false
-  false
+  full?(board) && !won?(board)
 end
 
 def over?(board)
-  return true if full?(board) || won?(board) || draw?(board)
+  won?(board) || full?(board)
 end
 
 def winner(board)
-  if won?(board)
-      board[won?(board)[0]]
+  if winning_combo = won?(board)
+    board[winning_combo.first]
   end
 end
